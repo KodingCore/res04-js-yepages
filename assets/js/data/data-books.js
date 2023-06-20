a0aad37e939e0774bdbc2e35da96c645dc7051a5
KodingCore <131252253+KodingCore@users.noreply.github.com>
2023-06-20 13:31:43 +0200

    next

--- assets/js/data/data-books.js
+++ assets/js/data/data-books.js
@@ -4,98 +4,98 @@ let bookList = [
         title : "Harry Potter à l'école des sorciers",
         genre : "Fantasy Jeunesse",
         author : "J.K. Rowling",
-        resume : "harry's yolo",
+        resume : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!",
         image : "assets/images/hp1.jpg"
     },
     {
         title : "Cartographie des nuages",
         genre : "Science-Fiction Adulte",
         author : "David Mitchell",
-        resume : "",
+        resume : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!",
         image : "assets/images/Cloud_atlas.jpg"
     },
     {
         title : "La communauté de l'anneau",
         genre : "Fantasy Adulte",
         author : "J.R.R Tolkien",
-        resume : "",
+        resume : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!",
         image : "assets/images/lotr.jpg"
     },
     {
         title : "Truth of the divine",
         genre : "Science-Fiction Adulte",
         author : "Lindsay Ellis",
-        resume : "",
+        resume : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!",
         image : "assets/images/truth-of-the-divine.jpeg"
     },
     {
         title : "La guerre des mondes",
         genre : "Science-Fiction Adulte",
         author : "H.G.Wells",
-        resume : "",
+        resume : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!",
         image : "assets/images/war-of-the-worlds.jpeg"
     },
     {
         title : "Ensemble c'est tout",
         genre : "Romance Adulte",
         author : "Ana Gavalda",
-        resume : "",
+        resume : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!",
         image : "assets/images/ensemble.jpeg"
     },
     {
         title : "Misery",
         genre : "Horreur Adulte",
         author : "Stephen King",
-        resume : "",
+        resume : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!",
         image : "assets/images/misery.jpg"
     },
     {
         title : "Pars vite et reviens tard",
         genre : "Polar Adulte",
         author : "Fred Vargas",
-        resume : "",
+        resume : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!",
         image : "assets/images/pars-vite.jpg"
     },
     {
         title : "Hunger Games part 1",
         genre : "Science-Fiction Jeunesse",
         author : "Suzanne Collins",
-        resume : "",
+        resume : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!",
         image : "assets/images/hunger-games.jpg"
     },
     {
         title : "Da Vinci Code",
         genre : "Polar Adulte",
         author : "Dan Brown",
-        resume : "",
+        resume : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!",
         image : "assets/images/da-vinci-code.jpg"
     },
     {
         title : "La huitième couleur",
         genre : "Fantasy Adulte",
         author : "Terry Pratchett",
-        resume : "",
+        resume : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!",
         image : "assets/images/8-couleur.jpeg"
     },
     {
         title : "La horde du Contrevent",
         genre : "Science-Fiction Adulte",
         author : "Alain Damasio",
-        resume : "",
+        resume : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!",
         image : "assets/images/horde_hd.jpg"
     },
     {
         title : "Les chroniques martiennes",
         genre : "Science-Fiction Adulte",
         author : "Ray Bradbury",
-        resume : "",
+        resume : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!",
         image : "assets/images/chroniques-martiennes.jpeg"
     },
     {
         title : "Le maitre du Haut-Chateau",
         genre : "Science-Fiction Adulte",
         author : "Philippe K. Dick",
-        resume : "",
+        resume : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias libero ex labore, quaerat asperiores. Aperiam eveniet, quidem aut dolores omnis magnam architecto mollitia inventore ipsum praesentium in saepe necessitatibus quaerat! Numquam quo quia quasi perspiciatis quaerat at ratione, molestias officia, itaque ea fugiat, mollitia repellendus blanditiis eveniet incidunt facere!",
         image : "assets/images/Le-Maitre-du-Haut-Chateau.jpg"
     }
 ];
--- assets/js/index.js
+++ assets/js/index.js
@@ -2,50 +2,107 @@ import { Library } from "./data/library.js";
 import { bookList } from "./data/data-books.js";
 
 window.addEventListener("DOMContentLoaded", function(){
-    const lib = document.getElementById("lib");
-    
-    let newShelf = new Library();
-    
-    newShelf.load(bookList);
-    
-    for (let book of newShelf.shelf) {
-        
-        let newArticle = document.createElement("article");
-        lib.appendChild(newArticle);
-        
-        console.log(book.image);
-        
+    const library = document.getElementById("library");
+    const discover = document.getElementById("discover");
+    const titrePage = document.getElementById("titrePage");
+    const tabCateg = document.getElementById("tabCateg");
+    const newShelf = new Library();
+    const subtitle = document.getElementById("subtitle");
+
+    function DisplayShelf(nbrBooks){
+        let i = 0;
+
+        newShelf.load(bookList);
+
+        for (let book of newShelf.shelf) {
+
+            let newArticle = document.createElement("article");
+            library.appendChild(newArticle);
+
+            console.log(book.image);
+
+            let imageElement = document.createElement("img");
+            imageElement.setAttribute("src", book.image);
+            newArticle.appendChild(imageElement);
+
+            let titreElement = document.createElement("h4");
+            let titreText = document.createTextNode(book.title);
+            newArticle.appendChild(titreElement);
+            titreElement.appendChild(titreText);
+
+            let genreElement = document.createElement("p");
+            let genreText = document.createTextNode(book.genre);
+            newArticle.appendChild(genreElement);
+            genreElement.appendChild(genreText);
+
+            let authorElement = document.createElement("p");
+            let authorText = document.createTextNode("par " + book.author);
+            newArticle.appendChild(authorElement);
+            authorElement.appendChild(authorText);
+
+            let btn = document.createElement("button");
+            let btnText = document.createTextNode("DECOUVRIR LE LIVRE");
+            newArticle.appendChild(btn);
+            btn.appendChild(btnText);
+
+            btn.addEventListener("click", SelectBook.bind(null, book));
+
+            if(i === nbrBooks-1){
+                break;
+            }
+            i++;
+        }
+    }
+
+    function SelectBook(book){
+        window.sessionStorage.setItem("title", book.title);
+
+        discover.removeChild(discover.firstChild);
+
+        titrePage.textContent = book.title;
+        subtitle.textContent = book.title;
+
+        let newDiscovering = document.createElement("article");
+        discover.appendChild(newDiscovering);
+
         let imageElement = document.createElement("img");
         imageElement.setAttribute("src", book.image);
-        newArticle.appendChild(imageElement);
-        
+        newDiscovering.appendChild(imageElement);
+
         let titreElement = document.createElement("h4");
         let titreText = document.createTextNode(book.title);
-        newArticle.appendChild(titreElement);
+        newDiscovering.appendChild(titreElement);
         titreElement.appendChild(titreText);
-        
+
         let genreElement = document.createElement("p");
         let genreText = document.createTextNode(book.genre);
-        newArticle.appendChild(genreElement);
+        newDiscovering.appendChild(genreElement);
         genreElement.appendChild(genreText);
-        
+
         let authorElement = document.createElement("p");
         let authorText = document.createTextNode("par " + book.author);
-        newArticle.appendChild(authorElement);
+        newDiscovering.appendChild(authorElement);
         authorElement.appendChild(authorText);
-        
-        let btn = document.createElement("button");
-        let btnText = document.createTextNode("DECOUVRIR LE LIVRE");
-        newArticle.appendChild(btn);
-        btn.appendChild(btnText);
 
+        let resumeElement = document.createElement("p");
+        let resumeText = document.createTextNode(book.resume);
+        newDiscovering.appendChild(resumeElement);
+        authorElement.appendChild(resumeText);
+
+        console.log(book.title);
     }
-    const tabCateg = document.getElementById("tabCateg");
-    for(let categorie of newShelf.getCategories()){
-        let newCategElement = document.createElement("li");
-        let newCategTextNode = document.createTextNode(categorie);
-        
-        tabCateg.appendChild(newCategElement);
-        newCategElement.appendChild(newCategTextNode);
+
+    function DisplayFilters(){
+        for(let categorie of newShelf.getCategories()){
+            let newCategElement = document.createElement("li");
+            let newCategTextNode = document.createTextNode(categorie);
+            
+            tabCateg.appendChild(newCategElement);
+            newCategElement.appendChild(newCategTextNode);
+        }
     }
+
+    DisplayShelf(50);
+    DisplayFilters();
+    
 })
